09e0d9b25439612f4069acfb65bb02a36b689910
Elson <huangyuhao@huangyuhaodeMacBook-Pro.local>
2016-10-31 12:42:52 +0800

    終極密碼

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+for _ in 1...10{
+    var i = arc4random_uniform(10)
+
+}
--- guessNumber/ViewController.swift
+++ guessNumber/ViewController.swift
@@ -1,24 +1,65 @@
-//
-//  ViewController.swift
-//  guessNumber
-//
-//  Created by 黃毓皓 on 2016/10/27.
-//  Copyright © 2016年 ice elson. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputNumber: UITextField!
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var bigOrSmall: UILabel!
+    @IBOutlet weak var minLabel: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    @IBOutlet weak var guessTime: UILabel!
+    
+    
+    var generate = 0
+    var life = 8
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        generate = Int(arc4random_uniform(100))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func yesOrno(_ sender: UIButton) {
+        if life > 0 {
+        let i = Int(inputNumber.text!)!
+        decide(test: i)
+        }else{
+            result.text = "不能再猜了"
+        }
+        
     }
+    
+    func decide(test:Int){
+        
+        let secrect = generate
+       
+        if test > secrect{
+            bigOrSmall.text = "在小一點"
+            life -= 1
+            maxLabel.text = String(test)
+            guessTime.text = String(life)
+            inputNumber.text = ""
+            
+        }else if test < secrect {
+            bigOrSmall.text = "在大一點"
+            life -= 1
+            minLabel.text = String(test)
+            guessTime.text = String(life)
+            inputNumber.text = ""
+        }else {
+            bigOrSmall.text = ""
+            result.text = "恭喜答對了 解答為\(secrect)"
+            result.isHidden = false
+            inputNumber.text = ""
+        }
+    }
+    @IBAction func Restart(_ sender: UIButton) {
+        result.isHidden = true
+        inputNumber.text = ""
+        life = 8
+       generate = Int(arc4random_uniform(100))
+        guessTime.text = String(life)
+        maxLabel.text = "100"
+        minLabel.text = "0"
+    }
+    
 
 
 }
